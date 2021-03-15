145c9aa6706b9b9087136bd699df6ed4e3a1d6a7
doojin <dmitry.papka@gmail.com>
2021-03-15 14:47:46 -0300

    implementing "Hello, world!" REST endpoint

--- src/math.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from './math'
-
-describe('math', () => {
-  describe('sum', () => {
-    test('returns sum of two numbers', () => {
-      expect(sum(1, 2)).toEqual(3)
-    })
-  })
-})
