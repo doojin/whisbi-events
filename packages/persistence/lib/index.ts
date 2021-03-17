b894122273d3cef3989e02cac25735b33a2700cd
doojin <dmitry.papka@gmail.com>
2021-03-17 15:56:33 -0300

    fixing persistence package

--- packages/persistence/lib/index.ts
+++ packages/persistence/lib/index.ts
@@ -1,4 +1,32 @@
+import path from 'path'
+import {
+  createConnection as createDatabaseConnection,
+  getConnection as getDatabaseConnection,
+  Connection
+} from 'typeorm'
 import User from './entity/User'
+
+export async function createConnection (
+  database: string,
+  username: string,
+  password: string,
+  synchronize: boolean = false
+): Promise<void> {
+  await createDatabaseConnection({
+    type: 'mysql',
+    database,
+    username,
+    password,
+    entities: [
+      path.join(__dirname, '../dist/entity/*.js')
+    ],
+    synchronize
+  })
+}
+
+export function getConnection (): Connection {
+  return getDatabaseConnection()
+}
 
 export {
   User
