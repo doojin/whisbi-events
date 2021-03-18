f5f640f2d69c91c160067ffaf223992bc0cc5212
doojin <dmitry.papka@gmail.com>
2021-03-18 10:48:23 -0300

    removing dummy endpoint

--- packages/api/lib/rest/endpoint/hello.ts
+++
@@ -1,7 +0,0 @@
-import { Handler, Request, Response } from 'express'
-
-const helloHandler: Handler = (req: Request, res: Response) => {
-  res.json({ message: 'Hello, world!' })
-}
-
-export default helloHandler
