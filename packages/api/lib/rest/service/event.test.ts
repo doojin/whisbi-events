bca524c0bfceb29037637d196bb27037dfeaa044
doojin <dmitry.papka@gmail.com>
2021-03-18 15:27:52 -0300

    throwing error when user tries to publish multiple non-draft events

--- packages/api/lib/rest/service/event.test.ts
+++ packages/api/lib/rest/service/event.test.ts
@@ -1,5 +1,6 @@
 import eventService from './event'
 import { getEventRepository, User } from '@whisbi-events/persistence'
+import HttpError from '../HttpError'
 
 jest.mock('@whisbi-events/persistence')
 
@@ -9,24 +10,42 @@ describe('event service', () => {
   beforeEach(() => {
     eventRepository = {
       create: jest.fn(),
-      save: jest.fn()
+      save: jest.fn(),
+      hasNonDraftUserEvents: jest.fn()
     };
 
     (getEventRepository as jest.Mock).mockReturnValue(eventRepository)
   })
 
   describe('create', () => {
-    test('creates user event', async () => {
-      const user: User = new User()
-
-      await eventService.create({ id: 13, headline: 'test event' }, user)
-
-      expect(eventRepository.create).toHaveBeenCalledTimes(1)
-      expect(eventRepository.create).toHaveBeenCalledWith({
-        headline: 'test event',
-        user
+    describe('user has no non-draft events', () => {
+      beforeEach(() => {
+        eventRepository.hasNonDraftUserEvents.mockResolvedValue(false)
       })
-      expect(eventRepository.save).toHaveBeenCalledTimes(1)
+
+      test('creates user event', async () => {
+        const user: User = new User()
+
+        await eventService.create({ id: 13, headline: 'test event' }, user)
+
+        expect(eventRepository.create).toHaveBeenCalledTimes(1)
+        expect(eventRepository.create).toHaveBeenCalledWith({
+          headline: 'test event',
+          user
+        })
+        expect(eventRepository.save).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    describe('user has non-draft event', () => {
+      beforeEach(() => {
+        eventRepository.hasNonDraftUserEvents.mockResolvedValue(true)
+      })
+
+      test('throws exception', async () => {
+        await expect(eventService.create({ id: 13, headline: 'test event' }, new User()))
+          .rejects.toEqual(new HttpError(400, 'User can have only one non-draft event at a time'))
+      })
     })
   })
 })
--- packages/api/lib/rest/service/event.ts
+++ packages/api/lib/rest/service/event.ts
@@ -1,10 +1,17 @@
 import { Event, getEventRepository, User } from '@whisbi-events/persistence'
+import HttpError from '../HttpError'
 
 export default {
   async create (eventData: Partial<Event>, user: User): Promise<void> {
     delete eventData.id
 
     const eventRepository = getEventRepository()
+    const hasNonDraftEvents = await eventRepository.hasNonDraftUserEvents(user.id)
+
+    if (hasNonDraftEvents) {
+      throw new HttpError(400, 'User can have only one non-draft event at a time')
+    }
+
     const event = eventRepository.create({ ...eventData, user })
     await eventRepository.save(event)
   }
