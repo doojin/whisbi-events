6b46962ae6f371f7c95edb038d5111360d9693e3
doojin <dmitry.papka@gmail.com>
2021-03-18 13:12:56 -0300

    creating REST service runner

--- packages/api/lib/rest/service.ts
+++ packages/api/lib/rest/service.ts
@@ -1,13 +1,21 @@
 import express from 'express'
-import helloHandler from './endpoint/hello'
+import passport from 'passport'
+import bodyParser from 'body-parser'
+import createEvent from './endpoint/createEvent'
+import authenticate from './authentication/index'
+import authenticated from './rules/authenticated'
 
 export default {
   start (port: number) {
     const router = express.Router()
-    router.get('/hello', helloHandler)
-
     const app = express()
+
+    app.use(bodyParser.json())
+    app.use(passport.initialize())
+    app.use(authenticate())
+
     app.use('/api/v1', router)
+    router.post('/event', authenticated, createEvent)
 
     return app.listen(port, () => {
       console.log(`REST service started on port: ${port}`)
