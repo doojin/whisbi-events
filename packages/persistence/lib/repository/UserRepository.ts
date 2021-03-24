5080ac050605f3a955bdbc88317627f86df91863
doojin <dmitry.papka@gmail.com>
2021-03-23 21:01:14 -0300

    adding cache on user-by-token queries

--- packages/persistence/lib/repository/UserRepository.ts
+++ packages/persistence/lib/repository/UserRepository.ts
@@ -6,6 +6,11 @@ export default class UserRepository extends Repository<User> {
   async findByTokenValue (accessToken: string): Promise<User|undefined> {
     return await this.createQueryBuilder('user')
       .innerJoin('user.token', 'token', 'token.value = :token', { token: accessToken })
+      // 1. Token values do not change frequently
+      // 2. Even if it changes, it cannot happen that the same token is assigned to another user
+      // 3. This query is used by middleware on every non-anonymous request
+      // So lets cache this query for a couple of minutes to remove a bit of stress from our database :)
+      .cache(120_000)
       .getOne()
   }
 }
